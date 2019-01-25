203fb6869919aa11b4eed3d4a04b377c4f69a7b1
Shnuer <Al.A.Kuznetsov@yandex.ru>
2019-01-25 18:50:18 +0300

    boud

--- TiltCV_detection/test1.py
+++ TiltCV_detection/test1.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 def nothing(x):
     pass
@@ -8,33 +9,26 @@ cap = cv2.VideoCapture(0)
 
 cv2.namedWindow('image')
 
-cv2.createTrackbar('clipLimit', 'image',20,1000, nothing)
-cv2.createTrackbar('tileGridSize', 'image',8,100, nothing)
+# cv2.createTrackbar('clipLimit', 'image',20,1000, nothing)
+# cv2.createTrackbar('tileGridSize', 'image',8,100, nothing)
 while (1):
 
-    cliptlimit = cv2.getTrackbarPos('clipLimit', 'image')
+    # cliptlimit = cv2.getTrackbarPos('clipLimit', 'image')
 
-    tiltegridsize = cv2.getTrackbarPos('tileGridSize', 'image')
+    # tiltegridsize = cv2.getTrackbarPos('tileGridSize', 'image')
     
-    cliptlimit = cliptlimit / 10
+    # cliptlimit = cliptlimit / 10
 
     _, frame = cap.read()
 
     new_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    edges = cv2.Canny(new_frame,100,200)
 
-    #adapt equalize
-    clahe = cv2.createCLAHE(clipLimit=cliptlimit, tileGridSize=(tiltegridsize,tiltegridsize)) 
-    cl1 = clahe.apply(new_frame)
+    cv2.imshow('new_frame',new_frame)
 
-    # sample equalize
-    equ = cv2.equalizeHist(new_frame)
-
-    res1 = np.hstack((new_frame,equ))
-    res2 = np.hstack((new_frame, cl1))
-    cv2.imshow('res1', res1)
-    cv2.imshow('image', res2)
-
+    cv2.imshow('edges',edges)
+    
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
